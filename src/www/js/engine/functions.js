5aa0e5bf5dc218aa6d26ee94be93d0cda819c0c7
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2019-01-23 15:07:55 +0100

    Updated functions

--- src/www/js/engine/functions.js
+++ src/www/js/engine/functions.js
@@ -1,5 +1,6 @@
 export class functions{
 
+	//General use
 	base64encode(str){
 		return window.btoa(unescape(encodeURIComponent(str)));
 	}
@@ -8,6 +9,76 @@ export class functions{
 		return decodeURIComponent(escape(window.atob(str)));
 	}
 
+	parseURLParams(url) {
+		var queryStart = url.indexOf("?") + 1,
+			queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+			query = url.slice(queryStart, queryEnd - 1),
+			pairs = query.replace(/\+/g, " ").split("&"),
+			parms = {}, i, n, v, nv;
+	
+		if (query === url || query === "") return;
+	
+		for (i = 0; i < pairs.length; i++) {
+			nv = pairs[i].split("=", 2);
+			n = decodeURIComponent(nv[0]);
+			v = decodeURIComponent(nv[1]);
+	
+			if (!parms.hasOwnProperty(n)) parms[n] = [];
+			parms[n].push(nv.length === 2 ? v : null);
+		}
+		return parms;
+	}
+
+	getDomain(){
+		return `${window.location.protocol}//${window.location.host}/`;
+	}
+
+	onExitFunction(message = "Are you sure you want to quit?"){
+		if(app.config.alert_exit){
+			return message;
+		}
+	}
+
+	confirmMessage(message = "Are you sure you want to quit?"){
+		if(app.config.alert_exit){
+			return confirm(message);
+		}else{
+			return true;
+		}
+	}
+
+	//Numbers
+	isNumeric(value){
+		return !isNaN(value - parseFloat(value));
+	}
+	
+	number_format(number, decimals, decPoint, thousandsSep) {
+	
+		number = (number + "").replace(/[^0-9+\-Ee.]/g, "");
+		var n = !isFinite(+number) ? 0 : +number;
+		var prec = !isFinite(+decimals) ? 0 : Math.abs(decimals);
+		var sep = (typeof thousandsSep === "undefined") ? "," : thousandsSep;
+		var dec = (typeof decPoint === "undefined") ? "." : decPoint;
+		var s = "";
+	
+		var toFixedFix = function (n, prec) {
+			var k = Math.pow(10, prec);
+			return "" + (Math.round(n * k) / k).toFixed(prec);
+		};
+	
+		s = (prec ? toFixedFix(n, prec) : "" + Math.round(n)).split(".");
+		if (s[0].length > 3) {
+			s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+		}
+		if ((s[1] || "").length < prec) {
+			s[1] = s[1] || "";
+			s[1] += new Array(prec - s[1].length + 1).join("0");
+		}
+	
+		return s.join(dec);
+	}
+
+	//UI/effects
 	message(message){
 
 		let html = "";
@@ -22,7 +93,7 @@ export class functions{
 
 		let fade_out_fun = this.fadeOut;
 
-		//Hide
+		//Hide + remove
 		setTimeout(function(){
 			fade_out_fun(message_div);
 		}, 2000)
@@ -63,108 +134,25 @@ export class functions{
 		tick();
 	}
 
-	parseURLParams(url) {
-		var queryStart = url.indexOf("?") + 1,
-			queryEnd   = url.indexOf("#") + 1 || url.length + 1,
-			query = url.slice(queryStart, queryEnd - 1),
-			pairs = query.replace(/\+/g, " ").split("&"),
-			parms = {}, i, n, v, nv;
-	
-		if (query === url || query === "") return;
-	
-		for (i = 0; i < pairs.length; i++) {
-			nv = pairs[i].split("=", 2);
-			n = decodeURIComponent(nv[0]);
-			v = decodeURIComponent(nv[1]);
-	
-			if (!parms.hasOwnProperty(n)) parms[n] = [];
-			parms[n].push(nv.length === 2 ? v : null);
-		}
-		return parms;
+	hide(el){
+		el.style.display = "none";
 	}
 
-	isNumeric(value){
-		return !isNaN(value - parseFloat(value));
-	}
-	
-	number_format(number, decimals, decPoint, thousandsSep) {
-	
-		number = (number + "").replace(/[^0-9+\-Ee.]/g, "");
-		var n = !isFinite(+number) ? 0 : +number;
-		var prec = !isFinite(+decimals) ? 0 : Math.abs(decimals);
-		var sep = (typeof thousandsSep === "undefined") ? "," : thousandsSep;
-		var dec = (typeof decPoint === "undefined") ? "." : decPoint;
-		var s = "";
-	
-		var toFixedFix = function (n, prec) {
-			var k = Math.pow(10, prec);
-			return "" + (Math.round(n * k) / k).toFixed(prec);
-		};
-	
-		s = (prec ? toFixedFix(n, prec) : "" + Math.round(n)).split(".");
-		if (s[0].length > 3) {
-			s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
-		}
-		if ((s[1] || "").length < prec) {
-			s[1] = s[1] || "";
-			s[1] += new Array(prec - s[1].length + 1).join("0");
-		}
-	
-		return s.join(dec);
+	show(el){
+		el.style.display = "";
 	}
 
-	onExitFunction(message = "Are you sure you want to quit?"){
-		if(app.config.alert_exit){
-			return message;
-		}
+	//HTML
+	insertAfter(target, html){
+		target.insertAdjacentHTML("afterend", html);
 	}
 
-	confirmMessage(message = "Are you sure you want to quit?"){
-		if(app.config.alert_exit){
-			return confirm(message);
-		}else{
-			return true;
-		}
+	insertBefore(target, html){
+		target.insertAdjacentHTML("beforebegin", html);
 	}
 
-	deleteSpacesAtEndAndBeginning(value){
-
-		if(value !== "" && value !== " " && typeof value === "string"){
-			
-			var i = 0;
-			var len = value.length;
-
-			//Spaces at the begining
-			for(i; i < len; i++){
-				if(value[i] === " "){
-					if(i === 0){
-						value = value.substr(1);
-						len = value.length;
-						i = -1;
-					}
-				}
-			}
-
-			//Spaces at the end
-			i = value.length;
-
-			for(i; i >= 0; i--){
-				if(value[i-1] === " "){
-					if(i === value.length){
-						value = value.substr(0, (i - 1));
-						i = value.length + 1;
-					}else{
-						break;
-					}
-				}
-			}
-		}       
-
-		return value;
-	}
-
-	getDomain(){
-		return `${window.location.protocol}//${window.location.host}/`;
+	removeElement(el){
+		el.parentNode.removeChild(el);
 	}
 
 }
