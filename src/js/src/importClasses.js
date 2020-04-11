a6cc205630135b3934633b777b92651c16236537
david baqueiro <davidbaqueiro@outlook.com>
2020-04-11 15:01:31 +0200

    added new file

---
+++ src/js/src/importClasses.js
@@ -0,0 +1,14 @@
+/**
+    Experiment here (WIP)
+    this files tries to import all classes in a folder, and define which classes
+    need to be imported in the app.classes scope
+*/
+
+// import { Person } from "./src/classes/Person.js";
+// import { Student } from "./src/classes/Student.js";
+// import { Teacher } from "./src/classes/Teacher.js";
+// import { Table } from "./src/classes/Table.js";
+
+// TODO: finish here
+
+
