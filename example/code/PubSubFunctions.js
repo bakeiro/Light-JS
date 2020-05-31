4762be4c0ab9bde24013d798ef05208877af0586
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2020-05-31 21:25:52 +0200

    minor update

--- example/code/PubSubFunctions.js
+++
@@ -1,5 +0,0 @@
-const pub_sub_events = {};
-pub_sub_events.createTeacher = ["classes.Table.createTeacherJS", "classes.Table.createTeacherHTML", "classes.Table.refreshData"];
-pub_sub_events.createStudent = ["classes.Table.createStudentJS", "classes.Table.createStudentHTML", "classes.Table.refreshData"];
-
-export default pub_sub_events;
--- example/example1/code/appCode.js
+++ example/example1/code/appCode.js
@@ -1,13 +1,26 @@
 
-// DocumentReady
+//lazy load
+export { Person } from "../classes/Person.js";
+export { Student } from "../classes/Student.js";
+export { Table } from "../classes/Table.js";
+export { Teacher } from "../classes/Teacher.js";
+
+// use pub/sub
+const pub_sub_events = {};
+pub_sub_events.createTeacher = ["classes.Table.createTeacherJS", "classes.Table.createTeacherHTML", "classes.Table.refreshData"];
+pub_sub_events.createStudent = ["classes.Table.createStudentJS", "classes.Table.createStudentHTML", "classes.Table.refreshData"];
+
+
+// events here
+
 document.addEventListener("DOMContentLoaded", () => {
   // Add dynamic content
   const divRight = d.$("#left_2");
   const html = `
     <div class="square">
-      <h4>Dynamic content</h4>
-      <br>
-      <button id="show_message">Message</button>
+    <h4>Dynamic content</h4>
+    <br>
+    <button id="show_message">Message</button>
     </div>`;
   divRight.innerHTML += html;
 });
